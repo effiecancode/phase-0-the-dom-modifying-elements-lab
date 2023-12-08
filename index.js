1aba718396f57baa3dac885e281ae3fdb9ddd480
effiecancode <faitheffie25@gmail.com>
2023-12-08 15:55:22 +0300

    all tests passed

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // DOM manipulation
+const mainElement = document.getElementById('main');
+mainElement.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+newHeader.textContent = 'Effie is the champion';
